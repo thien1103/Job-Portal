5334b9c5e0fc25fea3c108019ec7d41d6dd08dec
SMT310 <shinizeam@gmail.com>
2025-03-21 20:18:02 +0700

    add script to package simplify API routes

--- backend/index.js
+++ backend/index.js
@@ -27,10 +27,10 @@ const PORT = process.env.PORT || 3000;
 
 
 // api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
+app.use("/user", userRoute);
+app.use("/company", companyRoute);
+app.use("/job", jobRoute);
+app.use("/application", applicationRoute);
 
 
 
